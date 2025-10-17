6180e09892a266b719bcc8417545b9da584d0f4f
chunchunzhao <chunchunzhao@gmail.com>
2025-10-16 20:56:51 -0400

    Add a blank map.

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// This line will print a message to the browser's developer console
+// It's a great way to confirm your script is loaded and running correctly!
+console.log("Welcome to ChessParents! The site is ready.");
+
+// Future code for the interactive map will go here.
